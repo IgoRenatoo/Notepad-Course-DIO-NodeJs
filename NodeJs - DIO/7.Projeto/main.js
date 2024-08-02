f07f2916b9298b26840c732b0e9645f7163ca58b
IgoRenatoo <igor.renato.oliveira@gmail.com>
2024-08-02 12:46:59 -0300

    docs: :page_facing_up: Anotações do modulo 4 do curso NodeJs - DIO

--- NodeJs - DIO/7.Projeto/main.js
+++
@@ -1,5 +0,0 @@
-import { connectDatabase , disconnectDatabase } from "./src/utils/database.js";
-(async function main(){
-  let dataBase = await connectDatabase('SQLite');
-  console.log('Status concluído ~>',dataBase[1])   
-})()
